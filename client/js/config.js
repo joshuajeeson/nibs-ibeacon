9c44c0c36e31b25421bee0056689006f1d5b3910
joshuajeeson <git@joshuajeeson.com>
2016-10-23 21:06:56 +0100

    membership types

--- client/js/config.js
+++ client/js/config.js
@@ -6,12 +6,12 @@ angular.module('nibs_ibeacon.config', [])
 
     .constant('STATUS_LABELS', [
         'Entry',
-        'Beginner',
+        'Seasoned',
         'Expert'
     ])
 
     .constant('STATUS_DESCRIPTIONS', [
         'As an Entry level subsciber, please choose your subscription details by clicking edit on the top right corner.',
-        'Trinitario (Trin-it-air-ee-yo)  is a hybrid combining the superior taste of the criollo bean with the resilience of the forastero bean. It’s not quite as rare as Criollo, but it’s close!',
-        'Criollo (Kree-oh-yo) is the most valued and rare type of cacao. Our highest level of Nibs members share the taste and sophistication of  select Crillolo pods.'
+        'Just like every season, keep up your good work and focus on your cereal.',
+        'You are the Expert. Let us know if you need us.'
     ]);
