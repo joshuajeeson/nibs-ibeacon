98aa79bcaec7d36542176aac79ac928514c66cfa
joshuajeeson <git@joshuajeeson.com>
2016-10-23 20:31:00 +0100

    update

--- client/js/config.js
+++ client/js/config.js
@@ -5,13 +5,13 @@ angular.module('nibs_ibeacon.config', [])
     .constant('FB_APP_ID','916692588342928')
 
     .constant('STATUS_LABELS', [
-        'Forastero',
-        'Trinitario',
-        'Criollo'
+        'Entry',
+        'Beginner',
+        'Expert'
     ])
 
     .constant('STATUS_DESCRIPTIONS', [
-        'Forastero (For-ah-stare-oh)  is a common base to all but the finest of chocolates.  Our newest Nibs members move as fast as Forastero pods grow.',
+        'As an Entry level subsciber, please choose your subscription details by clicking edit on the top right corner.',
         'Trinitario (Trin-it-air-ee-yo)  is a hybrid combining the superior taste of the criollo bean with the resilience of the forastero bean. It’s not quite as rare as Criollo, but it’s close!',
         'Criollo (Kree-oh-yo) is the most valued and rare type of cacao. Our highest level of Nibs members share the taste and sophistication of  select Crillolo pods.'
     ]);
